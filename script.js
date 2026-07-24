38a4fdaef7e678d8a4076869378d5eb28d0255d1
Arom Vinicius Hano Barsallo <aromvhb@gmail.com>
2026-07-23 21:26:16 -0300

    refactor: reorganize responsive CSS architecture and optimize hero/about mobile layout

--- script.js
+++ script.js
@@ -23,3 +23,46 @@ function revealOnScroll() {
 window.addEventListener("scroll", revealOnScroll);
 
 window.addEventListener("load", revealOnScroll);
+
+const progressBar = document.querySelector(".progress-bar");
+
+window.addEventListener("scroll", () => {
+
+    const scroll =
+        window.scrollY;
+
+    const height =
+        document.documentElement.scrollHeight -
+        window.innerHeight;
+
+    const progress =
+        (scroll / height) * 100;
+
+    progressBar.style.width =
+        `${progress}%`;
+
+});
+
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+window.addEventListener("scroll", () => {
+  let currentSection = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 120;
+    const sectionHeight = section.offsetHeight;
+
+    if (window.scrollY >= sectionTop) {
+      currentSection = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === `#${currentSection}`) {
+      link.classList.add("active");
+    }
+  });
+});
